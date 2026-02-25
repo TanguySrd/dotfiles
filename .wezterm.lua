f76abce2a3de6d5693354710706cb4956d48f5c2
Tanguy Serrand <tanguy.serrand@lunatech.nl>
2026-02-25 10:08:28 +0100

    Fix wezterm font style

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,10 +1,6 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
-
--- This is where you actually apply your config choices
 
 config.colors = {
 	foreground = "#CBE0F0",
@@ -18,8 +14,8 @@ config.colors = {
 	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
 }
 
-config.font = wezterm.font("MesloLGS Nerd Font")
-config.font_size = 12
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 16
 
 config.enable_tab_bar = false
 
@@ -27,5 +23,4 @@ config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.7
 config.macos_window_background_blur = 10
 
--- and finally, return the configuration to wezterm
 return config
