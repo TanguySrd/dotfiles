fe2ec05d9945b7d1fe1b32f4ffd6b91da0fb0352
Tanguy Serrand <tanguy.serrand@lunatech.nl>
2026-03-03 11:13:37 +0100

    Wezterm opacity

--- .wezterm.lua
+++ .wezterm.lua
@@ -20,7 +20,7 @@ config.font_size = 16
 config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.7
+config.window_background_opacity = 0.8
 config.macos_window_background_blur = 10
 
 config.window_padding = {
